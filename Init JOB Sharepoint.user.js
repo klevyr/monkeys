3469ae1e6f9ab24e47f0f3fda9a612c66376c71a
Klevy <klever.ramon@gmail.com>
2019-04-16 11:51:40 -0500

    replace variable hour by minute

--- Init JOB Sharepoint.user.js
+++ Init JOB Sharepoint.user.js
@@ -12,15 +12,15 @@
     'use strict';
 
     var fireornot = true;
-    var _hour_enter = (Math.floor(Math.random() * 6) + 1) + 30;
+    var _min_enter = (Math.floor(Math.random() * 6) + 1) + 30;
     setInterval(function(){
-        console.log("Hora de entrada: " + _hour_enter)
+        console.log("Hora de entrada: " + _min_enter)
         var dt = new Date();
         if(dt.getDay() >= 1 && dt.getDay() <= 5){
             console.log("Eval Date: " + dt)
             var elem;
             // Entry to work
-            if( dt.getHours() == '08' && dt.getMinutes() == _hour_enter ){
+            if( dt.getHours() == '08' && dt.getMinutes() == _min_enter ){
                 elem = document.getElementById("ctl00_m_g_a818293e_949a_4a0e_b8a6_ddbffce13a1f_ctl00_toolBarTbl_RightRptControls_ctl00_ctl00_diidIOSaveItem");
                 if (typeof elem.onclick == "function") {
                     if(fireornot){
